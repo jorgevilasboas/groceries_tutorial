1b108443c576607c335ac1cc1991ab2ea6d8e332
jorgevilasboas <jorge.sergipe@gmail.com>
2018-03-09 13:43:22 -0300

    Adds ActivityIndicator

--- app/views/list/list.js
+++ app/views/list/list.js
@@ -22,15 +22,18 @@ var pageData = new observableModule.fromObject({
     grocery: ""
 });
 
-exports.loaded = function(args) {
+exports.loaded = function (args) {
     page = args.object;
     page.bindingContext = pageData;
 
     groceryList.empty();
-    groceryList.load();
+    pageData.set("isLoading", true);
+    groceryList.load().then(function () {
+        pageData.set("isLoading", false);
+    });
 };
 
-exports.add = function() {
+exports.add = function () {
     // Check for empty submissions
     if (pageData.get("grocery").trim() === "") {
         dialogsModule.alert({
@@ -43,7 +46,7 @@ exports.add = function() {
     // Dismiss the keyboard
     page.getViewById("grocery").dismissSoftInput();
     groceryList.add(pageData.get("grocery"))
-        .catch(function() {
+        .catch(function () {
             dialogsModule.alert({
                 message: "An error occurred while adding an item to your list.",
                 okButtonText: "OK"
