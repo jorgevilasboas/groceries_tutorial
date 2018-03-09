d72b54c323a6c32dfa3ec1cde95205a1405ef34d
jorgevilasboas <jorge.sergipe@gmail.com>
2018-03-08 22:17:53 -0300

    Adds chapter 3

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -1,11 +1,26 @@
-exports.loaded = function() {
-    console.log("hello");
+var observableModule = require("data/observable");
+
+var user = new observableModule.fromObject({
+    email: "user@domain.com",
+    password: "password"
+});
+
+var frameModule = require("ui/frame");
+
+var page;
+var email;
+
+exports.loaded = function(args) {
+    page = args.object;
+    page.bindingContext = user;
 };
 
-exports.signIn = function() {
-    alert("Signing in");
+exports.signIn = function () {
+    email = page.getViewById("email");
+    console.log(email.text);
 };
 
-exports.register = function() {
-    alert("Registering");
+exports.register = function () {
+    var topmost = frameModule.topmost();
+    topmost.navigate("views/register/register");
 };
