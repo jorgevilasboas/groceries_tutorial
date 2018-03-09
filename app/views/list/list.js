b52d4bc8abe5af7a574b557fcd62ff52e372b4f7
jorgevilasboas <jorge.sergipe@gmail.com>
2018-03-08 23:08:25 -0300

    Adds add button

--- app/views/list/list.js
+++ app/views/list/list.js
@@ -1,3 +1,4 @@
+// Continua no 4.5
 var GroceryListViewModel = require("../../shared/view-models/grocery-list-view-model");
 
 var dialogsModule = require("ui/dialogs");
@@ -15,8 +16,10 @@ var page;
 */
 
 var groceryList = new GroceryListViewModel([]);
+
 var pageData = new observableModule.fromObject({
-    groceryList: groceryList
+    groceryList: groceryList,
+    grocery: ""
 });
 
 exports.loaded = function(args) {
@@ -25,4 +28,28 @@ exports.loaded = function(args) {
 
     groceryList.empty();
     groceryList.load();
+};
+
+exports.add = function() {
+    // Check for empty submissions
+    if (pageData.get("grocery").trim() === "") {
+        dialogsModule.alert({
+            message: "Enter a grocery item",
+            okButtonText: "OK"
+        });
+        return;
+    }
+
+    // Dismiss the keyboard
+    page.getViewById("grocery").dismissSoftInput();
+    groceryList.add(pageData.get("grocery"))
+        .catch(function() {
+            dialogsModule.alert({
+                message: "An error occurred while adding an item to your list.",
+                okButtonText: "OK"
+            });
+        });
+
+    // Empty the input field
+    pageData.set("grocery", "");
 };
